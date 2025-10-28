68b4195ae1c9bb77e4560fa9cc361973474e1e90
guilhermesptech <guilherme.doliveira@sptech.school>
2025-10-28 20:17:26 -0300

    feat: criar model para entidade Parameteur

--- src/main/java/domain/model/entity/ParameteurEntity.java
+++ src/main/java/domain/model/entity/ParameteurEntity.java
@@ -1,2 +1,29 @@
-package domain.model.entity;public class ParameteurEntity {
+package domain.model.entity;
+
+public class ParameteurEntity {
+    private Integer id;
+    private Integer maximumAlert;
+
+    public ParameteurEntity(Integer id, Integer maximumAlert) {
+        this.id = id;
+        this.maximumAlert = maximumAlert;
+    }
+
+    public ParameteurEntity() {}
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getMaximumAlert() {
+        return this.maximumAlert;
+    }
+
+    public void setMaximumAlert(Integer maximumAlert) {
+        this.maximumAlert = maximumAlert;
+    }
 }
