741c04aa377f851c7a2f9deca55d2aee4103b43a
guilhermesptech <guilherme.doliveira@sptech.school>
2025-11-02 17:58:46 -0300

    feat: dependencia que criei para centralizar o processamento em diretórios

--- src/main/java/infrastructure/folder/FolderProcessor.java
+++ src/main/java/infrastructure/folder/FolderProcessor.java
@@ -1,2 +1,20 @@
-package infrastructure.folder;public class FolderProcessor {
+package infrastructure.folder;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class FolderProcessor {
+    public static void cleanFolder(Path folder) {
+        try {
+            if (Files.exists(folder) && Files.isDirectory(folder)) {
+                try (var files = Files.newDirectoryStream(folder)) {
+                    for (Path file : files) {
+                        Files.deleteIfExists(file);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.getMessage();
+        }
+    }
 }
